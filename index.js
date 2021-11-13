abd4157285d91dc84f7b39e0366ceec4e0ac0712
sajolbhadra <sajolbhadra@gmail.com>
2021-11-13 19:26:24 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {MongoClient} = require('mongodb')
 const cors = require('cors')
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
-const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
